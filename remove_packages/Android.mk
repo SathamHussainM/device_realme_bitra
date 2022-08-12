b9c3942fc7450aac93baa476f5dd11b540b53df7
SathamHussainM <sathamhussain.m11@gmail.com>
2022-08-12 12:20:06 +0000

    bitra: Remove few more unwanted packages

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AppDirectedSMSService CarrierSetup ConnMO DCMO DMService MyVerizonServices RecorderPrebuilt Maps YouTube Drive PrebuiltGmail arcore SoundAmplifierPrebuilt SafetyHubPrebuilt OBDM_Permissions obdm_stub Showcase SprintDM SprintHM USCCDM VZWAPNLib VzwOmaTrigger
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AppDirectedSMSService CarrierSetup ConnMO DCMO DMService MyVerizonServices RecorderPrebuilt Photos Maps YouTube YouTubeMusicPrebuilt Drive PrebuiltGmail arcore SoundAmplifierPrebuilt SCONE SafetyHubPrebuilt OBDM_Permissions obdm_stub Showcase SprintDM SprintHM USCCDM VZWAPNLib VzwOmaTrigger GoogleTTS CarrierMetrics DiagnosticsToolPrebuilt NgaResources ScribePrebuilt MicropaperPrebuilt NgaResources PrebuiltGoogleTelemetryTvp Tycho VZWAPNLib YouTube talkback TrichromeLibrary TrichromeLibrary-Stub
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
