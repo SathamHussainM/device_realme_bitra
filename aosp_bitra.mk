cbcbc31800657c31c79f2da40b79759bd08178e8
SathamHussainM <sathamhussain.m11@gmail.com>
2022-09-23 06:54:06 +0000

    bitra: Add common_full_phone.mk
    Signed-off-by: SathamHussainM <sathamhussain.m11@gmail.com>

--- aosp_bitra.mk
+++ aosp_bitra.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Elixir stuff
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_PIXEL_CHARGER := true
 TARGET_SUPPORTS_QUICK_TAP := true
