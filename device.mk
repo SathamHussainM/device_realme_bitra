6c03676bfc1277c74c02aa89db61844895486d2a
cristianvaz <cristianvaz81993@gmail.com>
2023-01-18 12:52:47 +0100

    bitra: Add powerhint.xml from stock

--- device.mk
+++ device.mk
@@ -50,6 +50,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 
+# Powerhint
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
 # Overlays
 PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
